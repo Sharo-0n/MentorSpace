58f9f0e0e7cbbea5ab02a3420163e47464e9ca1b
Sharo-0n <sharoon@ucsd.edu>
2018-03-10 17:41:53 -0800

    nav bar worksgit add *

--- function/index.js
+++ function/index.js
@@ -1,3 +1,41 @@
 function myFunction() {
 	alert("I am an alert box!");
 }
+
+var panes = ['Home', 'FindConnections', 'ChatHistory', 'Settings'];
+var currentPane = 'Home';
+$('.pane_change[pane-link=FindConnections]').hide();
+$('.pane_change[pane-link=ChatHistory]').hide();
+$('.pane_change[pane-link=Settings]').hide();
+
+function showHome(){
+	$('.pane_change[pane-link=FindConnections]').hide();
+	$('.pane_change[pane-link=ChatHistory]').hide();
+	$('.pane_change[pane-link=Settings]').hide();
+	$('.pane_change[pane-link=Home]').show();
+
+	// $('.pane_change[pane-link=Home]').fadeIn({
+ //      width: '200px'
+ //    }, 300);
+}
+function showFindConnections(){
+	$('.pane_change[pane-link=Home]').hide();
+	$('.pane_change[pane-link=ChatHistory]').hide();
+	$('.pane_change[pane-link=Settings]').hide();
+	$('.pane_change[pane-link=FindConnections]').show();
+	// .fadeIn({
+ //      width: '200px'
+ //    }, 300);
+}
+function showChatHistory(){
+	$('.pane_change[pane-link=Home]').hide();
+	$('.pane_change[pane-link=FindConnections]').hide();
+	$('.pane_change[pane-link=Settings]').hide();
+	$('.pane_change[pane-link=ChatHistory]').show();
+	// fadeIn({
+ //      width: '200px'
+ //    }, 300);
+}
+function shwoSettings(){
+
+}
